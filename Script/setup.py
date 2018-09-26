e34fa1fa2ea4792c8d86dcac1f24d658b5db8866
teamzz111 <aflr@outlook.com>
2018-09-26 01:20:09 -0500

    Adding setup

---
+++ Script/setup.py
@@ -0,0 +1,14 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable("Snake.py", base = "Win32GUI")]
+
+build_exe_options = {"packages": ["pygame"], "include_files": ["OpenSans-Light.ttf","25.jpg","icon.png","FONDO jp-01.png","song.ogg","Sonig.ogg"]}
+
+
+cx_Freeze.setup(
+    name = "Snake",
+    version = "0.1",
+    description = "Juego proporcionado por Andrés largo con los jóvenes programdores",
+    options = {"build_exe": build_exe_options},
+    executables = executables
+)
