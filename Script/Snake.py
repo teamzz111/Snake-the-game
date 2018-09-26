a9733bfc81c6a46c4a9de7c5a8299041e2ea1c46
teamzz111 <aflr@outlook.com>
2018-09-25 22:43:29 -0500

    Game Over menu

--- Script/Snake.py
+++ Script/Snake.py
@@ -23,7 +23,39 @@ ancho = 800
 altura = 500
 cuadro = 15
 
+font = pygame.font.SysFont('Arial', 25)
+
+def message_to_screen(msg, color):
+    textSur, textRect = text_objetos(msg, color)
+    textRect.center = (800) , (500)
+    superficie.blit(textSur,textRect)
+def text_objetos(text, color):
+    textSuperficie = font.render(text, True, color)
+    return textSuperficie, textSuperficie.get_rect()
+def gameLoop():
+    pausado = True
+    while pausado: 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameExit = False
+                mover_x = 300
+                mover_y = 300
+                mover_x_cambio = 0
+                mover_y_cambio = 0
 while not gameExit:
+
+        while gameOver == True:
+            superficie.fill(blanco)
+        message_to_screen("Se acabó la partida", Negro, -50)
+        message_to_screen("Para continuar presionar C, terminar presionar Q", Rojo, 50)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key pygame.K_q:
+                    gameExit = True
+                    gameOver = False
+                if event.key == pygame.K_c:
+                    gameLoop()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
